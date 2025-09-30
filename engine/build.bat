d8ef8e5a6458e649b829a45ea6b62d6033b354b2
Valentino <mevex98@gmail.com>
2025-09-30 14:44:50 +0200

    Removed MSVC's permissive mode from the build.bat files

--- engine/build.bat
+++ engine/build.bat
@@ -15,7 +15,7 @@ set Defines=-DKIWI_SLOW -DKIWI_ENGINE_EXPORTS
 rem set IgnoredWarnings=/wd4201 /wd4505
 set Libraries=user32.lib winmm.lib %VULKAN_SDK%\Lib\vulkan-1.lib
 
-set CompilerFlags=/nologo /MDd /fp:fast /GR- /EHa- /Od /Oi /WX /W4 /FC /Zi /LD %IncludeFolders% %Defines% %IgnoredWarnings%
+set CompilerFlags=/nologo /MDd /fp:fast /GR- /EHa- /Od /Oi /WX /W4 /FC /Zi /LD /permissive- %IncludeFolders% %Defines% %IgnoredWarnings%
 set LinkerFlags=/nologo /incremental:no /opt:ref %Libraries%
 
 cl %CompilerFlags% %FileNames% /Fe%Assembly% /link %LinkerFlags%
--- testbed/build.bat
+++ testbed/build.bat
@@ -14,7 +14,7 @@ set IncludeFolders=/I../testbed/src /I../engine/src/
 set Defines=-DKIWI_SLOW -DKIWI_ENGINE_EXPORTS
 rem set IgnoredWarnings=/wd4201 /wd4505
 
-set CompilerFlags=/nologo /MTd /fp:fast /GR- /EHa- /Od /Oi /WX /W4 /FC /Zi %IncludeFolders% %Defines% %IgnoredWarnings%
+set CompilerFlags=/nologo /MTd /fp:fast /GR- /EHa- /Od /Oi /WX /W4 /FC /Zi /permissive- %IncludeFolders% %Defines% %IgnoredWarnings%
 set LinkerFlags=/nologo /incremental:no /opt:ref engine.lib
 
 
