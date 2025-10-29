549b192277925b9492795fa19eac33aa788e92c7
Valentino <mevex98@gmail.com>
2025-10-29 22:21:39 +0100

    Changed the build.bat files to perform a unified build

--- engine/build.bat
+++ engine/build.bat
@@ -2,9 +2,11 @@
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-set FileNames=
+break>..\bin\engine_unity_build.cpp
+
 for /R %%f in (*.cpp) do (
-	set FileNames=!FileNames! %%f
+	set "IncludeFile=#include "%%f""
+	echo !IncludeFile! >> ..\bin\engine_unity_build.cpp
 )
 
 pushd ..\bin
@@ -12,12 +14,12 @@ pushd ..\bin
 set Assembly=Engine
 set IncludeFolders=/I../engine/src /I%VULKAN_SDK%/Include
 set Defines=-DKIWI_SLOW -DKIWI_ENGINE_EXPORTS
-rem set IgnoredWarnings=/wd4201 /wd4505
+:: set IgnoredWarnings=/wd4201 /wd4505
 set Libraries=user32.lib winmm.lib %VULKAN_SDK%\Lib\vulkan-1.lib
 
 set CompilerFlags=/nologo /MDd /fp:fast /GR- /EHa- /Od /Oi /WX /W4 /FC /Zi /LD /permissive- %IncludeFolders% %Defines% %IgnoredWarnings%
 set LinkerFlags=/nologo /incremental:no /opt:ref %Libraries%
 
-cl %CompilerFlags% %FileNames% /Fe%Assembly% /link %LinkerFlags%
+cl %CompilerFlags% engine_unity_build.cpp /Fe%Assembly% /link %LinkerFlags%
 
 popd
--- testbed/build.bat
+++ testbed/build.bat
@@ -2,9 +2,11 @@
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-set FileNames=
+break>..\bin\testbed_unity_build.cpp
+
 for /R %%f in (*.cpp) do (
-	call set FileNames=!FileNames! %%f
+	set "IncludeFile=#include "%%f""
+	echo !IncludeFile! >> ..\bin\testbed_unity_build.cpp
 )
 
 pushd ..\bin
@@ -12,12 +14,12 @@ pushd ..\bin
 set Assembly=Testbed
 set IncludeFolders=/I../testbed/src /I../engine/src/
 set Defines=-DKIWI_SLOW -DKIWI_ENGINE_EXPORTS
-rem set IgnoredWarnings=/wd4201 /wd4505
+:: set IgnoredWarnings=/wd4201 /wd4505
 
 set CompilerFlags=/nologo /MTd /fp:fast /GR- /EHa- /Od /Oi /WX /W4 /FC /Zi /permissive- %IncludeFolders% %Defines% %IgnoredWarnings%
 set LinkerFlags=/nologo /incremental:no /opt:ref engine.lib
 
 
-cl %CompilerFlags% %FileNames% /Fe%Assembly% /link %LinkerFlags%
+cl %CompilerFlags% testbed_unity_build.cpp /Fe%Assembly% /link %LinkerFlags%
 
 popd
